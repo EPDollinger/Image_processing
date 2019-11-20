a8fa2ed92ca8343bb69187a922df669f00708a78
Emmanuel <edolling@uci.edu>
2019-11-19 18:48:44 -0800

    Changed what the message is from 'writing picture 1,2,3' etc to just 'writing pictures'

--- Supplemental Code/BWandColorOut.m
+++ Supplemental Code/BWandColorOut.m
@@ -2,21 +2,20 @@ function BWandColorOut(names,BW_out_array,I_struct,Parameters)
 
 if strcmp(Parameters.store_option,'BW') == 1
     
+    'Writing BW pictures to folder'
+    
    for image = 1:length(names)
 
         imwrite(BW_out_array.(names{image}),[Parameters.output_location_BWpictures names{image} '.png'])
         
-        [names{image} ' written to file']
-        
    end
 else
+    'Writing paired pictures to folder'
     for image = 1:length(names)
 
         pic = imfuse(I_struct.(names{image}),BW_out_array.(names{image}),'montage');
 
         imwrite(pic,[Parameters.output_location_BWpictures names{image} 'montage.png'])
-        
-        [names{image} ' paired written to file']
 
     end
 
